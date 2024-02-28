9e06c89ab45a3e1841e8f0d0a871a446dd098b80
jra <julien.gault.50@gmail.com>
2024-02-28 16:24:24 +0100

    outer loop : display the whole string of the table if there is no dot

--- MSLearn.CSharpDoWhile.Challenge2.Project3/Program.cs
+++ MSLearn.CSharpDoWhile.Challenge2.Project3/Program.cs
@@ -1,1 +1,12 @@
 ﻿string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+int periodLocation;
+
+foreach (string myString in myStrings)
+{
+    periodLocation = myString.IndexOf(".");
+    if (periodLocation == -1)
+    {
+        Console.WriteLine(myString);
+        continue;
+    }
+}
